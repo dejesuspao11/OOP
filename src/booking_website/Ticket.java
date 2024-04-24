99f4f3e0cf867656260c3271b0b36a9364343315
pao <dejesuspao111@gmail.com>
2024-04-24 09:28:57 -0600

    submitting exam

---
+++ src/booking_website/Ticket.java
@@ -0,0 +1,32 @@
+package booking_website;
+
+import java.util.Date;
+import java.util.Random;
+
+public class Ticket {
+		private String ticketID;
+		private String flightID;
+		private String userInfo;
+		Date flightDate;
+		int ticketPrice;
+		
+		public Ticket(String flightID, String userInfo, Date flightDate, int ticketPrice) {
+			this.ticketID =
+					
+String generateTicketID() {
+			Random randomNumber = new Random(999);
+			int ticketNumber = random.nextInt(1000);
+			return "RE-" + ticketNumber;
+			
+			
+			
+			
+			
+	  
+		
+		
+	}
+
+
+
+}
