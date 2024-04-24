fac821b254e0c94e0f87bc956268c511beba8cc3
dejesuspao11 <122827247+dejesuspao11@users.noreply.github.com>
2024-04-24 16:43:44 -0600

    Add files via upload
    submit test

---
+++ Booking/src/booking_website/Flight.java
@@ -0,0 +1,11 @@
+package booking_website;
+
+public class Flight { 
+	String flightID;
+	int seatsTotal = 40;
+	int ticketPrice;
+	
+		
+	}
+
+s
---
+++ Booking/src/booking_website/FlightManager.java
@@ -0,0 +1,27 @@
+package booking_website;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class FlightManager {
+    private List<Flight> flights;
+    private Map<String, String>flightMap;
+    private Map<String, Ticket> Ticket;
+    
+    
+public FlightManager() {
+	flights = new ArrayList<>();
+	tickets = new HashMap<>();
+	
+	
+	private void initializeFlight
+	
+}
+    
+
+
+
+
+}
